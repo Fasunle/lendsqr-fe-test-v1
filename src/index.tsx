75811049cee6f6237fc644280fcbd819866412e6
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-05 12:35:40 +0100

    feat: wrapp App component with router context

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as AppRouter } from 'react-router-dom';
 //
 import './styles/main.scss';
 import App from './App';
-import React from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot( document.getElementById( 'root' ) as HTMLElement );
 root.render(
   <React.StrictMode>
-    <App />,
+    <AppRouter>
+      <App />
+    </AppRouter>
   </React.StrictMode>,
 );
 
