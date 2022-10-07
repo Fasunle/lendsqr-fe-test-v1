7e21504cef7c061a93c8b5c4f3d4eb827f09d6f2
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-07 07:25:32 +0100

    refactor: prettier format

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+const Panel = () => {
+  return (
+    <aside className='sidebar'>
+      <ul>
+        <h3>Tell the all</h3>
+        <li>A girl</li>
+        <li>A white note</li>
+        <li>Wolf killers</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Panel;
