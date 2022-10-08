1a571c87dd7019bc02aae7e463f95d8eed8978d3
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-08 22:30:44 +0100

    fix: make  functional

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,14 +1,17 @@
 import { UserProfile } from '../components/user';
 import leftArrowLogo from '../assets/icons/arrow-left.svg';
+import { Link } from 'react-router-dom';
 
 const UserPage = () => {
   return (
     <div className='page user'>
-      <div className='page-title text-2xl user-page-title'>
-        <span className='leftArrorContainer'>
-          <img src={leftArrowLogo} alt='back to users' />
-        </span>
-        <span className='text-xl title'>Back to Users</span>
+      <div className='page-title text-2xl'>
+        <Link className='user-page-title' to='/users'>
+          <span className='leftArrorContainer'>
+            <img src={leftArrowLogo} alt='back to users' />
+          </span>
+          <span className='text-xl title'>Back to Users</span>
+        </Link>
       </div>
       <UserProfile
         accountNumber={9912345678}
