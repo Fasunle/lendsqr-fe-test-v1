4b02b107ee1c52efece70381e09ff0c218d33e6e
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-08 12:20:30 +0100

    feat: analytic cards

--- src/components/content/Card.tsx
+++ src/components/content/Card.tsx
@@ -10,10 +10,8 @@ const Card = ({ imgUrl, alt, status, numberOfUsers }: CartPropType) => (
     <div className='card__header'>
       <img src={imgUrl} alt={alt || status} />
     </div>
-    <div className='card__body'>
-      <p className='status'>{status}</p>
-      <b className='number-of-users'>{numberOfUsers}</b>
-    </div>
+    <p className='card__body--status'>{status}</p>
+    <b className='card__body--number-of-users'>{numberOfUsers}</b>
   </div>
 );
 
