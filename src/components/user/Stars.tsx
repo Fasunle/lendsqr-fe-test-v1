fa4af429c99ee1e0e590e49c94a3ba159da0adf5
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-08 17:07:39 +0100

    fix: div element can not be p child

--- src/components/user/Stars.tsx
+++ src/components/user/Stars.tsx
@@ -6,13 +6,13 @@ type StarType = {
   rating: number;
 };
 
-export const Stars = ({ rating: number }: StarType) => {
+export const Stars = ( { rating: number }: StarType ) => {
   // handle the logic here
   return (
-    <div className='stars'>
+    <span className='stars'>
       <img src={fullStarIcon} alt='full star' />
       <img src={emptyStarIcon} alt='empty star' />
       <img src={emptyStarIcon} alt='empty star' />
-    </div>
+    </span>
   );
 };
