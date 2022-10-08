cd86a230ba877a0a0e4ec7dd31ebdb95a6eba082
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-08 22:11:13 +0100

    feat: add page title to user page

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,8 +1,15 @@
 import { UserProfile } from '../components/user';
+import leftArrowLogo from '../assets/icons/arrow-left.svg';
 
 const UserPage = () => {
   return (
     <div className='page user'>
+      <div className='page-title text-2xl user-page-title'>
+        <span className='leftArrorContainer'>
+          <img src={leftArrowLogo} alt='back to users' />
+        </span>
+        <span className='text-xl title'>Back to Users</span>
+      </div>
       <UserProfile
         accountNumber={9912345678}
         balance='₦ 200,000.00'
